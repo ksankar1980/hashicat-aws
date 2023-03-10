c298983b5f8ed6693ef4b033bb8efde1a99382b2
Kart <ksankar@live.com.au>
2023-03-10 03:09:57 +0000

    test

--- main.tf
+++ main.tf
@@ -125,9 +125,10 @@ resource "aws_instance" "hashicat" {
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
-  key = "Department"
-  value = "devops"
+  
   tags = {
+    Department = "Devops"
+    Billable = "True"
     Name = "${var.prefix}-hashicat-instance"
   }
 }
